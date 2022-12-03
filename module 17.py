49988b52b93a5486983bf51bedf97c8a4e4927fe
YuliaEO <113613661+YuliaEO@users.noreply.github.com>
2022-12-03 12:38:46 +0300

    Update module 17.py

--- module 17.py
+++ module 17.py
@@ -1,15 +1,13 @@
 array = [int(x) for x in input("Введите числа от 1 до 999 в любом порядке, через пробел: ").split()]
 
-def merge_sort(array):  # "разделяй"
-    if len(array) < 2:  # если кусок массива равен 2,
-        return array[:]  # выход из рекурсии
+def merge_sort(array): 
+    if len(array) < 2: 
+        return array[:]  
     else:
-        middle = len(array) // 2  # ищем середину
-        left = merge_sort(array[:middle])  # рекурсивно делим левую часть
-        right = merge_sort(array[middle:])  # и правую
-        return merge(left, right)  # выполняем слияние
-
-
+        middle = len(array) // 2  
+        left = merge_sort(array[:middle])  
+        right = merge_sort(array[middle:]) 
+      
 def merge(left, right):
     result = []
     i, j = 0, 0
@@ -36,16 +34,15 @@ print(merge_sort(array))
 
 
 def binary_search(array, element, left, right):
-    if left > right:  # если левая граница превысила правую,
-        return False  # значит элемент отсутствует
+    if left > right:  
+        return False 
 
-    middle = (right + left) // 2  # находимо середину
-    if array[middle] == element:  # если элемент в середине,
-        return middle  # возвращаем этот индекс
-    elif element < array[middle]:  # если элемент меньше элемента в середине
-        # рекурсивно ищем в левой половине
+    middle = (right + left) // 2  
+    if array[middle] == element: 
+        return middle  
+    elif element < array[middle]: 
         return binary_search(array, element, left, middle - 1)
-    else:  # иначе в правой
+    else: 
         return binary_search(array, element, middle + 1, right)
 
 
@@ -56,9 +53,9 @@ while True:
             raise Exception
         break
     except ValueError:
-        print("Нужно ввести число!")
+        print("Нужно ввести число.)
     except Exception:
-        print("Неправильный диапазон!")
+        print("Неправильный диапазон.")
 
 
 print(binary_search(array, element, 0,  len(array)))
