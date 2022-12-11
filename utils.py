4223d30120eaf82ec23aea8cabe2d6de9da2fc0a
YuliaEO <113613661+YuliaEO@users.noreply.github.com>
2022-12-11 20:22:18 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,21 +2,20 @@ import requests
 
 import json
 
+from config import keys
+
+
 class ConvertionException(Exception):
     pass
 class CryptoConverter:
     @staticmethod
     def convert(quote: str, base: str, amount: str):
-        values = message.text.split(' ')
-        if len(values) != 3:
-            raise ConvertionException('Слишком много параметров')
-
-        quote, base, amount = values
 
         if quote == base:
             raise ConvertionException(f'Невозможно перевести одинаковые валюты{base}.')
 
         try:
+
             quote_ticker = keys[quote]
         except KeyError:
             raise ConvertionException(f'Не удалось обработать валюту {quote}')
@@ -29,6 +28,7 @@ class CryptoConverter:
         except ValueError:
             raise ConvertionException(f'Не удалось обработать количество {amount}')
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+
         total_base = json.loads(r.content)[keys[base]]
 
         return total_base
