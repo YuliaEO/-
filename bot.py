b5db052edf37927dccdbaded5459990a94ddfb89
YuliaEO <113613661+YuliaEO@users.noreply.github.com>
2022-12-11 20:21:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,9 +29,22 @@ def values(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
-    total_base = CryptoConverter.convert(quote, base, amount)
+    try:
+        values = message.text.split(' ')
 
-    text = f'Цена {amount} {quote} в {base} - {total_base}'
-    bot.send_message(message.chat.id,text)
+        if len (values) !=3:
+            raise ConvertionException ("Слишком много параметров.")
+
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message,f'Ошибка пользователя\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+
+    else:
+
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id,text)
 
 bot.polling()
